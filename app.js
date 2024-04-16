b12c36ef80e6be0d4c11387de94e7d8bc55316ab
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-04-16 20:59:02 +0530

    environment variables updation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DB_URL).then(()=>{console.log("MongoDB Connected")});
 
